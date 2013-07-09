e9a43a24e0551c06dc53f78962a0d823ddfc704b
Emmanuel Ah-kouen <emmanuel.ahkouen@gmail.com>
2013-07-09 14:30:56 +0000

    Edited for index.html

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = fs.read('index.html').toString();
+  response.send(text);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
