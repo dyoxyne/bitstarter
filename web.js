589aaede1acee4c8981862f35d78428144823683
Emmanuel Ah-kouen <emmanuel.ahkouen@gmail.com>
2013-07-09 14:35:08 +0000

    Edited for index.html 2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var text = fs.read('index.html').toString();
+  var text = fs.readFileSync('index.html').toString();
   response.send(text);
 });
 
